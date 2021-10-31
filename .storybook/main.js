33670cd6bbe4152f6fdb65787234757763482f7c
Попович Олег Игоревич <popovich.oi@esphere.ru>
2021-10-31 20:10:57 +0300

    storybook

---
+++ .storybook/main.js
@@ -0,0 +1,11 @@
+const path = require('path');
+
+module.exports = {
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-actions',
+    '@storybook/addon-essentials',
+    '@storybook/addon-knobs',
+  ],
+};
